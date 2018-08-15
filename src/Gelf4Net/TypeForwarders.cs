b293e0bda10bda4c5e8b8cd8e45d3f79f99694fe
Will Speak <will.speak@crispthinking.com>
2018-08-15 09:42:56 +0100

    Add Type Forwarder for New Appender
    This makes the new appender consistent with the other async ones.

--- src/Gelf4Net/TypeForwarders.cs
+++ src/Gelf4Net/TypeForwarders.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 
 [assembly:TypeForwardedTo(typeof(Gelf4Net.Layout.GelfLayout))]
 [assembly:TypeForwardedTo(typeof(Gelf4Net.Appender.GelfHttpAppender))]
+[assembly:TypeForwardedTo(typeof(Gelf4Net.Appender.AsyncGelfHttpAppender))]
 [assembly:TypeForwardedTo(typeof(Gelf4Net.Appender.GelfUdpAppender))]
 [assembly:TypeForwardedTo(typeof(Gelf4Net.Appender.AsyncGelfUdpAppender))]
 [assembly:TypeForwardedTo(typeof(Gelf4Net.Appender.GelfAmqpAppender))]
